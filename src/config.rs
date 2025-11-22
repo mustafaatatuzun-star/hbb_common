3d724e96056a5975b0c0aaaea90057d7a16922f0
mustafaatatuzun-star <mustafa.atatuzun@gmail.com>
2025-11-22 19:00:34 +0200

    Update config.rs
    semra ayarları eklendi

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["id.uzak.semra.com.tr"];
+pub const RS_PUB_KEY: &str = "wLjVDK0wAuZnxyfqCzQY0ADmw5hJ8y+7MFa6wxd0q5c=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
